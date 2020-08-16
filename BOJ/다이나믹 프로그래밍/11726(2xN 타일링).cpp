89730a63d9e62102627b635c13491b270562f9c6
ss-hoon <46196555+ss-hoon@users.noreply.github.com>
2020-08-16 21:57:25 +0900

    11726(2*N 타일링)

---
+++ BOJ/다이나믹 프로그래밍/11726(2xN 타일링).cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int cache[1001] = { 0,1,2 };
+int main() {
+	int test;
+	cin >> test;
+	for (int i = 3; i <= test; i++)	cache[i] = (cache[i - 1] + cache[i - 2]) % 10007;
+	cout << cache[test] << "\n";
+	return 0;
+}
