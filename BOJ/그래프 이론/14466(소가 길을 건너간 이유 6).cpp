863c0bc2e297ec13b4b23aadcb9d0c415aa08de3
ss-hoon <46196555+ss-hoon@users.noreply.github.com>
2021-03-03 23:11:03 +0900

    14466(소가 길을 건너간 이유 6)

--- BOJ/그래프 이론/14466(소가 길을 건너간 이유 6).cpp
+++ BOJ/그래프 이론/14466(소가 길을 건너간 이유 6).cpp
@@ -2,27 +2,23 @@
 #include <vector>
 #include <cstring>
 using namespace std;
-int N, K, R, dstY, dstX, result;
-bool visit[101][101], isRoad[101][101][4];
+int N, K, R, result, p[10300];
+bool isRoad[101][101][4];
 int dx[4] = { 0, 1, 0, -1 };
 int dy[4] = { -1, 0, 1, 0 };
 vector< pair<int, int> > cow;
 
-bool dfs(int y, int x) {
-	if (y == dstY && x == dstX)	return true;
-	visit[y][x] = true;
+int find(int x) {
+	if (x == p[x])	return x;
+	return p[x] = find(p[x]);
+}
 
-	for (int i = 0; i < 4; i++) {
-		int Y = y + dy[i];
-		int X = x + dx[i];
+void merge(int x, int y) {
+	x = find(x);
+	y = find(y);
 
-		if (Y < 1 || Y > N || X < 1 || X > N)	continue;
-		if (visit[Y][X])	continue;
-		if (isRoad[y][x][i])	continue;
-
-		if (dfs(Y, X))	return true;
-	}
-	return false;
+	if (x == y)	return;
+	p[y] = x;
 }
 
 int main() {
@@ -33,7 +29,6 @@ int main() {
 		int r1, r2, c1, c2;
 		cin >> r1 >> c1 >> r2 >> c2;
 
-		// 길의 시작점에서 끝점의 방향 체크
 		for (int i = 0; i < 4; i++) {
 			if (r1 + dy[i] == r2 && c1 + dx[i] == c2) {
 				isRoad[r1][c1][i] = true;
@@ -45,21 +40,33 @@ int main() {
 		}
 	}
 
+	// init
+	for (int i = 1; i <= (N + 1) * (N + 1); i++)	p[i] = i;
+
+	// union-find
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			for (int k = 0; k < 4; k++) {
+				int y = i + dy[k];
+				int x = j + dx[k];
+				if (y < 1 || y > N || x < 1 || x > N || isRoad[i][j][k])	continue;
+
+				merge(find(i * N + j), find(y * N + x));
+			}
+		}
+	}
+
+	// 각각의 소가 있는 땅이 같은 부모인지 확인
+	// 다르면 길을 통해서 지나가야하므로 result++
 	for (int i = 0; i < K; i++) {
 		int r, c;
 		cin >> r >> c;
 		cow.push_back({ r, c });
-	}
-
-	for (int i = 0; i < K - 1; i++) {
-		for (int j = i + 1; j < K; j++) {
-			memset(visit, 0, sizeof(visit));
-			dstY = cow[j].first;	dstX = cow[j].second;
-
-			// dfs 결과 false로 반환된 것을 카운팅
-			if (!dfs(cow[i].first, cow[i].second))	result++;
+		for (int j = 0; j < i; j++) {
+			if (find(cow[i].first * N + cow[i].second) != find(cow[j].first * N + cow[j].second))	result++;
 		}
 	}
+
 	cout << result << "\n";
 	return 0;
 }
