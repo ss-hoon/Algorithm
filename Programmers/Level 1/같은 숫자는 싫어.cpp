3cb00995f608aedcae7f3e1881c51db2a0a4cf20
ss-hoon <46196555+ss-hoon@users.noreply.github.com>
2021-03-14 15:28:29 +0900

    같은 숫자는 싫어

---
+++ Programmers/Level 1/같은 숫자는 싫어.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) {
+    arr.erase(unique(arr.begin(), arr.end()), arr.end());
+    vector<int> answer = arr;
+    return answer;
+}
