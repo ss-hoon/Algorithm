760960493bcf06255209b3deaa7c83bc86789db6
ss-hoon <46196555+ss-hoon@users.noreply.github.com>
2021-03-16 09:53:04 +0900

    문자열 내 마음대로 정렬하기

---
+++ Programmers/Level 1/문자열 내 마음대로 정렬하기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int idx;
+bool compare(string a, string b){
+    if(a[idx] == b[idx])    return a < b;
+    return a[idx] < b[idx];
+}
+
+vector<string> solution(vector<string> strings, int n) {
+    idx = n;
+    sort(strings.begin(), strings.end(), compare);
+    return strings;
+}
