2653c6d8f977be151bd795dbad19ce9ff9cd2b8e
ss-hoon <lshoon1128@naver.com>
2023-03-27 21:53:45 +0900

    11660(구간 합 구하기 5)

---
+++ BOJ/다이나믹 프로그래밍/11660(구간 합 구하기 5).java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] board = new int[N+1][N+1];
+        int[][] sum = new int[N+1][N+1];
+        for (int i=1; i<=N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j=1; j<=N; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i=1; i<=N; i++) {
+            for (int j=1; j<=N; j++) {
+                sum[i][j] = sum[i-1][j] + sum[i][j-1] - sum[i-1][j-1] + board[i][j];
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (M-- > 0) {
+            st = new StringTokenizer(br.readLine());
+
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            sb.append(sum[x2][y2] - sum[x1-1][y2] - sum[x2][y1-1] + sum[x1-1][y1-1]).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
